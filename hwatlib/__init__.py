35327df59ebcfa8bc890a9f8f80cebc9ea300a94
n0tabdu11ah <muhammadabdullah8040@gmail.com>
2025-09-06 02:42:25 +0500

    Create __init__.py

---
+++ hwatlib/__init__.py
@@ -0,0 +1,16 @@
+"""
+hwatlib - A penetration testing wrapper library by hwatSauce
+Includes:
+- recon
+- web
+- exploit
+- post_exploit
+"""
+
+from .recon import *
+from .web import *
+from .exploit import *
+from .postex import *
+
+__version__ = "0.1.0"
+__author__ = "hwatSauce"
