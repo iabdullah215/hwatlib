fe5631309a2d1c46d1c93b2af2fc0d6ec1d21cea
!abdu11ah <muhammadabdullah8040@gmail.com>
2026-01-05 17:18:24 +0500

    Release v0.2.0

--- hwatlib/__init__.py
+++ hwatlib/__init__.py
@@ -36,5 +36,5 @@ __all__ = [
     "__version__",
 ]
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "HwatSauce"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hwatlib",
-    version="0.1.0",
+    version="0.2.0",
     package_dir={"": "hwatlib"},
     packages=find_packages(where="hwatlib"),
     install_requires=[
